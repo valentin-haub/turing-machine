1fe3a23abb82936b78f005be3e1458048f74e39c
Valentin <valentin.haub@mni.thm.de>
2026-01-02 00:36:19 +0100

    Optimize BasicOperations (bug fix on shift operation)

--- src/main/java/de/thm/mni/oop/BasicOperations.java
+++ src/main/java/de/thm/mni/oop/BasicOperations.java
@@ -6,12 +6,14 @@ class BasicOperations {
     // Diese Methode dekrementiert eine Binärzahl
     public static void decreaseBinary(String init){
         TM tm = new TM(init, "q0");
+
         tm.addTransition("q0", '0', '0', Move.RIGHT, "q0");
         tm.addTransition("q0", '1', '1', Move.RIGHT, "q0");
         tm.addTransition("q0", tm.EMPTY, tm.EMPTY, Move.LEFT, "q1");
         tm.addTransition("q1", '0', '1', Move.LEFT, "q1");
         tm.addTransition("q1", '1', '0', Move.RIGHT, "HALT");
         tm.addTransition("q1", tm.EMPTY, tm.EMPTY, Move.RIGHT, "HALT");
+
         tm.run();
         System.out.println(tm);
     }
@@ -19,12 +21,14 @@ class BasicOperations {
     // Diese Methode inkrementiert eine Binärzahl
     public static void increaseBinary(String init){
         TM tm = new TM(init, "q0");
+
         tm.addTransition("q0", '0', '0', Move.RIGHT, "q0");
         tm.addTransition("q0", '1', '1', Move.RIGHT, "q0");
         tm.addTransition("q0", tm.EMPTY, tm.EMPTY, Move.LEFT, "q1");
         tm.addTransition("q1", '1', '0', Move.LEFT, "q1");
         tm.addTransition("q1", '0', '1', Move.RIGHT, "HALT");
         tm.addTransition("q1", tm.EMPTY, tm.EMPTY, Move.RIGHT, "HALT");
+
         tm.run();
         System.out.println(tm);
     }
@@ -32,6 +36,7 @@ class BasicOperations {
     // Schiebt alle '1' nach rechts auf
     public static void shiftOnesToTheRight(String init){
         TM tm = new TM(init, "q0");
+
         tm.addTransition("q0", '0', '0', Move.RIGHT, "q0");
         tm.addTransition("q0", '1', '1', Move.RIGHT, "q0");
         tm.addTransition("q0", tm.EMPTY, tm.EMPTY, Move.LEFT, "q1");
@@ -44,5 +49,8 @@ class BasicOperations {
         tm.addTransition("q3", '0', '0', Move.RIGHT, "q3");
         tm.addTransition("q3", '1', '1', Move.LEFT, "q4");
         tm.addTransition("q4", '0', '1', Move.LEFT, "q1");
+
+        tm.run();
+        System.out.println(tm);
     }
 }
