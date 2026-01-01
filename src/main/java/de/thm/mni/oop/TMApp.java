43e6e358fb65505328b5ecfd54f074a20200ab96
Valentin <valentin.haub@mni.thm.de>
2026-01-01 22:54:20 +0100

    Modified TMApp.java to a demo

--- src/main/java/de/thm/mni/oop/TMApp.java
+++ src/main/java/de/thm/mni/oop/TMApp.java
@@ -1,28 +1,24 @@
 package de.thm.mni.oop;
 
-class TMApp{
-    public static void main(String[] args){
-        TuringMachine tm1 = TM.init('#', 6, "#11000#", "S");
+public class TMApp {
+    public static void main(String[] args) {
+        System.out.println("========== TURING MACHINE DEMO ==========" + "\n");
+        System.out.println("This demo shows how the machine works in multiple scenarios." + "\n\n");
+
+        // decrease
+        System.out.println("--- Scenario 1: Binary Decrement (110 -> 101) ---");
+        BasicOperations.decreaseBinary("110");
+        System.out.println("\n");
+
+        // increase
+        System.out.println("--- Scenario 2: Binary Increment (101 -> 110) ---");
+        BasicOperations.increaseBinary("101");
+        System.out.println("\n");
+
+        // shiftOnes
+        System.out.println("--- Scenario 3: Shift Ones to the Right (1101 -> 0111) ---");
+        BasicOperations.shiftOnesToTheRight("1101");
+        System.out.println("\n");
         
-        tm1.addTransition("S", '#', '#', Move.LEFT, "S");
-        tm1.addTransition("S", '1', '0', Move.RIGHT, "R");
-        tm1.addTransition("S", '0', '1', Move.LEFT, "L");
-
-        tm1.addTransition("R", '0', '0', Move.RIGHT, "R");
-        tm1.addTransition("R", '1', '1', Move.RIGHT, "R");
-        tm1.addTransition("R", '#', '#', Move.LEFT, "W");
-
-        tm1.addTransition("W", '1', '1', Move.RIGHT, "HALT");
-        tm1.addTransition("W", '0', '0', Move.RIGHT, "HALT");
-        tm1.addTransition("W", '#', '#', Move.RIGHT, "HALT");
-
-        tm1.addTransition("L", '0', '1', Move.LEFT, "L");
-        tm1.addTransition("L", '1', '0', Move.RIGHT, "R");
-        tm1.addTransition("L", '#', '#', Move.RIGHT, "R");
-
-        while(!tm1.getStatus().equals( "HALT")){
-            tm1 = tm1.step();
-            System.out.println(tm1.getTape() + " " + tm1.getStatus());
-        }
     }
 }
